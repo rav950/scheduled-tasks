82668b14b4e68c7be3297cccacf083ea53db9001
rav950 <rav950py@gmail.com>
2026-05-14 15:47:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,3 +36,40 @@ if today_tuple in birthdays_dict:
             to_addrs=birthday_person["email"],
             msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
+
+#####
+
+import requests
+from twilio.rest import Client
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+weather_parameters = {
+    "lat": 52.281940,
+    "lon": -1.584470, #latlong.net
+    "appid": api_key,
+    "cnt": 4,    # only 1st 4 items in data
+}
+
+response = requests.get(url=OWM_Endpoint, params=weather_parameters)
+response.raise_for_status()
+#print(response.status_code)
+weather_data = response.json()   # https://jsonviewer.stack.hu/
+#weather_list = weather_data["list"]
+
+will_rain = False
+for weather_period in weather_data["list"]:
+    weather_id = weather_period["weather"][0]["id"]
+    if int(weather_id) < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_="whatsapp:+14155238886",
+        body="It's going to rain today. Remember to bring an umbrella",
+        to="whatsapp:+447710594086"
+    )
